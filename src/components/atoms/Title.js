65e006595ccef7f88533a6cdc68eaae183a8d9ac
Rusel Dotdot <dotdot.russel@gmail.com>
2023-01-19 12:59:50 +0800

    Add data.json file

--- src/components/atoms/Title.js
+++ src/components/atoms/Title.js
@@ -1,7 +1,7 @@
-export default function Title() {
+export default function Title(props) {
   return (
     <div className="content-container">
-      <h1 className="title">ONLINE PORTFOLIO</h1>
+      {/* <h1 className="title">ONLINE PORTFOLIO</h1>
       <p className="description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec
         libero sollicitudin, tincidunt erat sed, fringilla leo. Phasellus
@@ -9,7 +9,9 @@ export default function Title() {
         tincidunt cursus odio purus quis diam. In hac habitasse platea dictumst.
         Integer pretium dignissim dictum. Ut ac ornare nulla. In eget nunc id
         quam blandit gravida et eget turpis.
-      </p>
+      </p> */}
+      <h1 className="title">{props.title}</h1>
+      <p className="description">{props.content}</p>
     </div>
   );
 }
--- src/components/organisms/OnlinePortfolio.js
+++ src/components/organisms/OnlinePortfolio.js
@@ -1,12 +1,16 @@
 import PictureSize from "../atoms/PictureSize";
 import Title from "../atoms/Title";
+import jsonData from "../../data/data.json";
 
 export default function NavigationBar() {
   return (
     <>
       <div className="landing-page-container">
         <PictureSize />
-        <Title />
+        <Title
+          title={jsonData[0].landingPage[0].title}
+          content={jsonData[0].landingPage[0].content}
+        />
       </div>
     </>
   );
